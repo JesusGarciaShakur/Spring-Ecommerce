96ede8e4ede2d16f1dcbd7cea53147dfb42debb3
jesus <jesusrguez970@gmail.com>
2025-04-06 14:40:14 -0600

    button save

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -1,14 +1,35 @@
 package com.ecommerce.controller;
 
+import org.slf4j.*;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecommerce.model.Product;
+
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+	
+	private final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(ProductController.class);
+	
 	@GetMapping("")
 	public String show() {
 		return "products/show.html";
 	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "products/create.html";
+	}
+	
+	@PostMapping("/save")
+	public String save(Product product) {
+		LOGGER.info("Este es el objeto de la vista {}", product);
+		return "redirect:/products";
+	}
+	
 }
