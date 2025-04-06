0cf388e24166d29ea504323153574714fbf48451
jesus <jesusrguez970@gmail.com>
2025-04-06 14:52:21 -0600

    Save in DB

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -1,13 +1,15 @@
 package com.ecommerce.controller;
 
 import org.slf4j.*;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.model.Product;
+import com.ecommerce.model.User;
+import com.ecommerce.service.ProductService;
 
 
 @Controller
@@ -15,6 +17,9 @@ import com.ecommerce.model.Product;
 public class ProductController {
 	
 	private final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(ProductController.class);
+	
+	@Autowired
+	private ProductService productService;
 	
 	@GetMapping("")
 	public String show() {
@@ -29,6 +34,9 @@ public class ProductController {
 	@PostMapping("/save")
 	public String save(Product product) {
 		LOGGER.info("Este es el objeto de la vista {}", product);
+		User u = new User(1,"","","","","","", null);
+		product.setUser(u);
+		productService.save(product);
 		return "redirect:/products";
 	}
 	
